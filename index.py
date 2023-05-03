890b84968cce9028e0d756b26a8caa274c8df98e
Ryan Deyo <rdeyo1129@gmail.com>
2023-05-03 19:28:54 -0400

    add icon file and path handle

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, url_for
+from flask import Flask, request, render_template, send_from_directory
 from dotenv import load_dotenv
 import os
 import openai
@@ -48,6 +48,11 @@ def chatGPT(flavor, prompt):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'staticFiles'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/topics')
 def topics():
